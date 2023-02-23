8264db46ed557fb5820a6cb342a4db4751991474
Anastasia Minchukova <anastasiaminchukova72@gmail.com>
2023-02-23 22:53:12 +0300

    Second homework

---
+++ ДЗ/23,02/task2.py
@@ -0,0 +1,5 @@
+# Пусть у нас есть массив слов pets = ["cat", "dog", "fish", "hamster", "parrot"].
+# Нужно вывести в консоль второе и четвертое слово через запятую.
+
+pets = ["cat", "dog", "fish", "hamster", "parrot"]
+print(pets[1], ", ", pets[3])
---
+++ ДЗ/23,02/task6.py
@@ -0,0 +1,15 @@
+# Пусть у нас есть словарь, где ключ это страна, а значение - столица:
+# countries_capitals = {
+#     'Belarus': 'Minsk',
+#     'Poland': 'Warsaw',
+#     'Great Britain': 'London',
+# }
+# Нужно вывести на экран, используя словарь, столицу Польши, а на следующей строке столицу Беларуси
+
+countries_capitals = {
+    'Belarus': 'Minsk',
+    'Poland': 'Warsaw',
+    'Great Britain': 'London',
+}
+print(f"{countries_capitals['Poland']} is the capital of Poland")
+print(f"{countries_capitals['Belarus']} is the capital of Belarus")
